f06403bcd837350e19323b0babbae92e0fd38980
stevenlazarre26 <stevenlazarre26@gmail.com>
2014-02-25 20:55:51 -0500

    here

---
+++ re/src/com/example/re/splash.java
@@ -0,0 +1,45 @@
+package com.example.re;
+
+import android.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+
+
+public class splash extends Activity {
+	
+	 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_splash);
+
+	
+    Thread timer = new Thread()
+    {
+      public void run()
+       {
+        try{
+        sleep(5000);	
+        }
+        catch(InterruptedException e){
+        	e.printStackTrace();
+        }
+        finally {
+        	Intent intent= new Intent("com.example.app.MainActivity");
+        	startActivity(intent);
+        }
+        }
+        };
+        timer.start();
+	}
+
+
+	
+
+
+	
+}
